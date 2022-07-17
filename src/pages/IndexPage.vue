ad5fd8533333d151244125f18e94362af8cdc9c9
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-07-17 01:17:07 -0700

    chore: formulario de pesquisa

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,23 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+       <q-input
+        v-model="search"
+        placeholder="Pesquisar"
+        dark
+        borderless
+        >
+        <template v-slot:before>
+          <q-icon name="my_location" />
+        </template>
+
+
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
@@ -12,6 +25,18 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup(){
+    return{
+      search: ''
+    }
+  }
 })
 </script>
+
+
+<style lang="sass">
+  .q-page
+    background: linear-gradient(to right, #136a8a, #267871)
+
+</style>
